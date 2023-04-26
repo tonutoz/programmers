2f9807389a84ad3500b8af0cced2d65c6d0eca1d
tonuto <tonuto@naver.com>
2023-04-26 15:39:22 +0900

    문제 푼거 추가

--- lib/src/test/java/coding/test/programmers/CodingTest.java
+++ lib/src/test/java/coding/test/programmers/CodingTest.java
@@ -1,5 +1,6 @@
 package coding.test.programmers;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class CodingTest {
@@ -9,7 +10,6 @@ public class CodingTest {
   void chaingeUpperAndLower() {
     String input = "aBcDeFg";
     StringBuffer sb = new StringBuffer();
-
     for (char c : input.toCharArray()) {
       if (Character.isUpperCase(c)) {
         sb.append(Character.toLowerCase(c));
@@ -18,6 +18,33 @@ public class CodingTest {
       }
     }
     System.out.println(sb.toString());
+
+  }
+
+  // 문자열 겹쳐 쓰기
+  @Test
+  void combineString() {
+    String myString = "He11oWor1d";
+    String overwriteString = "lloWorl";
+    int s = 2;
+    String answer = "";
+    String subString = myString.substring(s, overwriteString.length() + s);
+    answer = myString.replace(subString, overwriteString);
+
+    System.out.println(answer);
+
+  }
+
+  // 원소들의 곱과 합
+  @Test
+  void solution() {
+    int[] input = {3, 4, 5, 2, 1};
+    int sum = Arrays.stream(input).sum();
+    sum = sum * sum;
+    int multiply = Arrays.stream(input).reduce(1, (a, b) -> a * b);
+
+
+    System.out.println(multiply < sum ? 1 : 0);
 
   }
 
